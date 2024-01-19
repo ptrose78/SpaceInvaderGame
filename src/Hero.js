5793619fe2c581c2c67643f4360435c1f001942f
Paul Rose <ptrose78@gmail.com>
2024-01-19 15:10:21 -0600

    Created Movable Hero

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+// Functional component named 'Hero', which receives props: moveHero and positionHero
+function Hero({moveHero, positionHero}) {
+    
+    return (
+        <div style={{position:"relative", left:`${positionHero}px`, width:"50px", height:"50px"}}>
+            <img src="space-invader-ship.png"  />
+        </div> 
+    );
+}
+export default Hero;
