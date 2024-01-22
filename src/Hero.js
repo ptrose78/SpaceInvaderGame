0d2c9816cc8b5b331013ab245a8a93892e001ab4
Paul Rose <ptrose78@gmail.com>
2024-01-22 06:34:21 -0600

    Created Shooting Bullets

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 // Functional component named 'Hero', which receives props: moveHero and positionHero
-function Hero({moveHero, positionHero}) {
-    
+function Hero({positionHero}) {
+
     return (
         <div style={{position:"relative", left:`${positionHero}px`, width:"50px", height:"50px"}}>
             <img src="space-invader-ship.png"  />
